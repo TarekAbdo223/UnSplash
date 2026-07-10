175c70eb56b234223e9f6b0ec3d50479e7206c3c
TarekAbdo223 <tarekk.abdo22@gmail.com>
2026-07-10 17:13:46 +0300

    added tanstack query get fn

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -1,6 +1,20 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 import React from "react";
 
+const url =
+  "https://api.unsplash.com/search/photos?client_id=dIrmzY5MjI2N20Xo-dTX61uOhkA1YVlB_JsYEdVpnmo&query=office>";
+
 const Gallery = () => {
+  const res = useQuery({
+    queryKey: ["images"],
+    queryFn: async () => {
+      const result = await axios.get(url);
+      return result.data;
+    },
+  });
+  console.log(res);
+
   return <div>Gallery</div>;
 };
 
