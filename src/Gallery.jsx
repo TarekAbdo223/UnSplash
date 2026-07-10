811b05a92317e87ab65bec4d5ae68ba65e409745
TarekAbdo223 <tarekk.abdo22@gmail.com>
2026-07-10 18:35:56 +0300

    added some fallBack

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 import React from "react";
 
 const url =
-  "https://api.unsplash.com/search/photos?client_id=dIrmzY5MjI2N20Xo-dTX61uOhkA1YVlB_JsYEdVpnmo&query=office>";
+  "https://api.unsplash.com/search/photos?client_id=dIrmzY5MjI2N20Xo-dTX61uOhkA1YVlB_JsYEdVpnmo&query=cat>";
 
 const Gallery = () => {
   const res = useQuery({
@@ -15,7 +15,46 @@ const Gallery = () => {
   });
   console.log(res);
 
-  return <div>Gallery</div>;
+  if (res.isLoading) {
+    return (
+      <section className="image-container">
+        <h4>Loading...</h4>
+      </section>
+    );
+  }
+  if (res.isError) {
+    return (
+      <section className="image-container">
+        <h4>There was an error....</h4>
+      </section>
+    );
+  }
+
+  const result = res.data.results;
+
+  if (result.length < 1) {
+    return (
+      <section className="image-container">
+        <h4>No results found...</h4>
+      </section>
+    );
+  }
+
+  return (
+    <section className="image-container">
+      {result.map((item) => {
+        const url = item?.urls?.regular;
+        return (
+          <img
+            src={url}
+            key={item.id}
+            alt={item.alt_description}
+            className="img"
+          />
+        );
+      })}
+    </section>
+  );
 };
 
 export default Gallery;
